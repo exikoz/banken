7c685c5cb9a9660e0ae24edaa181fad4d54155db
alexanderjson95 <alexjson1995@hotmail.com>
2025-09-16 16:36:50 +0100

    added account poco

---
+++ bank/Account.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bank
+{
+    internal class Account
+    {
+        public int id {  get; set; }
+        public int user_id { get; set; }
+        public int accountNumber { get; set; }
+        public decimal balance { get; set; }
+           
+        public DateTime timestamp { get; set; }
+
+    }
+}
