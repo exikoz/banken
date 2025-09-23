32ad9ed5cafe2db153c38ba4887345ed0d1b32a2
JotsoChas <joco.borghol@chasacademy.se>
2025-09-23 15:13:32 +0200

    created the methots in account

--- bank/Account.cs
+++ bank/Account.cs
@@ -6,14 +6,48 @@ using System.Threading.Tasks;
 
 namespace bank
 {
-    internal class Account
+    public class Account
     {
-        public int Id {  get; set; }
-        public int UserId { get; set; }
-        public int AccountNumber { get; set; }
-        public decimal Balance { get; set; }
-           
-        public DateTime Timestamp { get; set; }
+
+        public string AccountNumber { get; set; }
+        public decimal Balance { get; private set; }
+        public User Owner { get; set; }
+
+        public void Deposit(decimal amount)
+        {
+            if (amount <= 0)
+            {
+                Console.WriteLine("Deposit failed: beloppet måste vara större än 0.");
+                return;
+            }
+
+            Balance += amount;
+            Console.WriteLine($"Deposit lyckades: +{amount} kr. Ny balans = {Balance} kr.");
+        }
+
+        public void Withdraw(decimal amount)
+        {
+            if (amount <= 0)
+            {
+                Console.WriteLine("Withdraw failed: beloppet måste vara större än 0.");
+                return;
+            }
+
+            if (amount > Balance)
+            {
+                Console.WriteLine("Withdraw failed: otillräckligt saldo.");
+                return;
+            }
+
+            Balance -= amount;
+            Console.WriteLine($"Withdraw lyckades: -{amount} kr. Ny balans = {Balance} kr.");
+        }
+
+
+        //public int Id {  get; set; }
+        //public int UserId { get; set; }
+        //public decimal Balance { get; set; }
+        //public DateTime Timestamp { get; set; }
 
     }
 }
